6b7d16bb6ce114ff841df67215fb596c59b6ec3b
sharmilasharmila <73405006+sharmilasharmila@users.noreply.github.com>
2021-05-02 23:08:21 +0530

    End of the Task

--- src/__test__/NumberOfEvents.test.js
+++ src/__test__/NumberOfEvents.test.js
@@ -14,30 +14,21 @@ describe('<NumberOfEvents /> component', () => {
 
   test('check input default value is equal to 32', () => {
     expect(
-      NumberOfEventsWrapper.find('.inputEvent').at(0).props().value
-    ).toEqual(32);
+      NumberOfEventsWrapper.find('.inputEvent').at(0).props().value).toEqual(32);
   });
 
   test('check placeholder to be Enter number of events', () => {
     expect(
-      NumberOfEventsWrapper.find('.inputEvent').at(0).props()
-        .placeholder
-    ).toEqual('Enter Number of Events');
+      NumberOfEventsWrapper.find('.inputEvent').at(0).props().placeholder).toEqual('Enter Number of Events');
   });
 
   test('check on change state', () => {
     const eventValue = { target: { value: 8 } };
-
-    NumberOfEventsWrapper.find('.inputEvent').simulate(
-      'change',
-      eventValue
-    );
+    NumberOfEventsWrapper.find('.inputEvent').simulate('change',eventValue);
     expect(NumberOfEventsWrapper.state('eventValue')).toBe(8);
   });
 
   test('check type is equal to number', () => {
-    expect(
-      NumberOfEventsWrapper.find('.inputEvent').at(0).props().type
-    ).toEqual('number');
+    expect(NumberOfEventsWrapper.find('.inputEvent').at(0).props().type).toEqual('number');
   });
 });
